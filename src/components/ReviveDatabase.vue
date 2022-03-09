79feb4528e44acd156bdbb9b34f710d97081efe9
Alive.Kuo <alegnadise@gmail.com>
2022-03-09 15:37:37 +0800

    chore(db): update isNew

--- src/components/ReviveDatabase.vue
+++ src/components/ReviveDatabase.vue
@@ -139,7 +139,7 @@
               x-small
               color="red"
               text-color="white"
-              v-if="item.isNew && (item.addDate === '2022/3/2')"
+              v-if="item.isNew && (item.addDate === '2022/3/9')"
             >
               NEW
             </v-chip>
